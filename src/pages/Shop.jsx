99388ced0a68b655e858f0b570ed4ba58d51e19f
Zandise Ntiyane <112590160+ZandiseOn100@users.noreply.github.com>
2025-04-16 16:13:55 +0200

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import IMAGES from "../images/IMAGES";
-
 
 const products = [
   { id: 1, name: 'On Budget Black Speakers', price: 550, image: IMAGES.image1 },
   { id: 2, name: 'Baller Speakers', price: 935, image: IMAGES.image2 },
   { id: 3, name: 'For Producers Speakers', price: 845, image: IMAGES.image3 },
 ];
- 
+
 function Shop({ addToCart }) {
   return (
     <div className="container">
-      <h1 className="">Shop</h1>
+      <h1>Shop</h1>
       <div className="grid">
-          {products.map((product) => (
-            <div key={product.id} className="border p-4">
-              <img src={product.image} alt={product.name} className="w-full h-auto product-card-shop" />
-              <h2>{product.name}</h2>
-              <p>R{product.price}</p>
-              <button onClick={() => addToCart(product)}>Add to Cart</button>
-            </div>
-          ))}
-        </div>
-    </div>    
+        {products.map((product) => (
+          <div key={product.id} className="border p-4">
+            <img src={product.image} alt={product.name} className="w-full h-auto product-card-shop" />
+            <h2>{product.name}</h2>
+            <p>R{product.price}</p>
+            <button onClick={() => addToCart(product)}>Add to Cart</button>
+          </div>
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default Shop;
