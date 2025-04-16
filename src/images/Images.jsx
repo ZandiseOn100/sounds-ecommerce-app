bdaf8382dbda8a02413e64ca1e32a9f7debfa3c1
Zandise Ntiyane <112590160+ZandiseOn100@users.noreply.github.com>
2025-04-16 16:26:02 +0200

    Update Images.jsx

--- src/images/Images.jsx
+++ src/images/Images.jsx
@@ -1,13 +1,13 @@
 const IMAGES = {
-    image1: new URL('/images/product1.png', import.meta.url).href,
-    image2: new URL('/images/product2.png', import.meta.url).href,
-    image3: new URL('/images/pngegg2.png', import.meta.url).href,
-    image4: new URL('/images/git.png', import.meta.url).href,
-    image5: new URL('/images/linkedin.png', import.meta.url).href,
-    image6: new URL('/images/phone.png', import.meta.url).href,
-    image7: new URL('/images/youtube.png', import.meta.url).href,
-    image8: new URL('/images/ig.png', import.meta.url).href,
-    image9: new URL('/images/x.png', import.meta.url).href,
-}
+  image1: new URL('./product1.png', import.meta.url).href,
+  image2: new URL('./product2.png', import.meta.url).href,
+  image3: new URL('./pngegg2.png', import.meta.url).href,
+  image4: new URL('./git.png', import.meta.url).href,
+  image5: new URL('./linkedin.png', import.meta.url).href,
+  image6: new URL('./phone.png', import.meta.url).href,
+  image7: new URL('./youtube.png', import.meta.url).href,
+  image8: new URL('./ig.png', import.meta.url).href,
+  image9: new URL('./x.png', import.meta.url).href,
+};
 
 export default IMAGES;
